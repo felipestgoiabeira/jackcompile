0e54d423ec1ea89dba5f777fe397e1189a5415dd
felipestgoiabeira <felipesg.12@outlook.com>
2021-07-03 15:47:51 -0300

    feat: struct for tokenizer

---
+++ lexical_analyzer/jack_tokenize.go
@@ -0,0 +1,37 @@
+package lexical_analyzer
+
+import (
+	"io/ioutil"
+	"log"
+	"regexp"
+)
+
+type IJackTokenizer interface {
+	hasMoreToken() bool
+	tokenType() int
+	keyWord() string
+	symbol() string
+	identifier() string
+	intVal() int
+	stringVal() string
+}
+
+type JackTokenizer struct {
+	tokens []string
+}
+
+func NewJackTokenizer(file string) *JackTokenizer {
+	j := new(JackTokenizer)
+	content, err := ioutil.ReadFile(file)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clearComents := regexp.MustCompile(`x(p*)y`)
+	sourceCode := clearComents.ReplaceAllString(string(content), " ")
+	getTokens := regexp.MustCompile(`(".*")|[a-zA-Z_]+[a-zA-Z0-9_]*|[0-9]+|[+|*|/|\-|{|}|(|)|\[|\]|\.|,|;|<|>|=|~]`)
+	j.tokens = getTokens.FindAllString(sourceCode, -1)
+
+	return j
+}
