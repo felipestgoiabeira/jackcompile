3df6d818cd8baa0f77de0213c9c1bb823cba9c04
felipestgoiabeira <felipesg.12@outlook.com>
2021-07-03 19:15:50 -0300

    test: tokenizer a file with comments

--- lexical_analyzer/jack_tokenize_test.go
+++ lexical_analyzer/jack_tokenize_test.go
@@ -2,9 +2,16 @@ package lexical_analyzer
 
 import "testing"
 
-func TestNewJackTokenizer(t *testing.T) {
-	jackTokenizer := NewJackTokenizer("../resources/test.jack")
+func TestNewJackTokenizerMustReturnANonEmptySlice(t *testing.T) {
+	jackTokenizer := NewJackTokenizer("../resources/simple_code.jack")
 	if len(jackTokenizer.tokens) == 0 {
 		t.Errorf("Array of tokens is empty")
+	}
+}
+
+func TestNewJackTokenizerMustReturnAEmptySlice(t *testing.T) {
+	jackTokenizer := NewJackTokenizer("../resources/only_comments.jack")
+	if len(jackTokenizer.tokens) > 0 {
+		t.Errorf("Array of tokens is not empty")
 	}
 }
