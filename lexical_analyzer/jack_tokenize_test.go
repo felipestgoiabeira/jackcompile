81b3020fd47788638bc11351b576704ed1157873
felipestgoiabeira <felipesg.12@outlook.com>
2021-07-03 23:07:03 -0300

    test:  xml tree test comparing with the expected result

--- lexical_analyzer/jack_tokenize_test.go
+++ lexical_analyzer/jack_tokenize_test.go
@@ -1,6 +1,12 @@
 package lexical_analyzer
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"testing"
+)
 
 func TestNewJackTokenizerMustReturnANonEmptySlice(t *testing.T) {
 	jackTokenizer := NewJackTokenizer("../resources/simple_code.jack")
@@ -14,4 +20,47 @@ func TestNewJackTokenizerMustReturnAEmptySlice(t *testing.T) {
 	if len(jackTokenizer.tokens) > 0 {
 		t.Errorf("Array of tokens is not empty")
 	}
+}
+
+func TestNewJackTokenizerMustBuildTheExpectedXMLTree(t *testing.T) {
+	jackTokenizer := NewJackTokenizer("../resources/complex_code.jack")
+	content, err := ioutil.ReadFile("../resources/xml/complex_code.xml")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	expectedResult := string(content)
+	fmt.Println("TESSSSSSSSSSSSSSTE")
+	fmt.Println(expectedResult)
+
+	xmlSpecialCharacters := map[string]string{
+		"<":  "&lt",
+		">":  "&gt",
+		"&":  "&#38",
+		"'":  "&#39",
+		"\"": "&#34",
+	}
+
+	var resultBuffer bytes.Buffer
+
+	resultBuffer.WriteString("<tokens>")
+
+	for _, token := range jackTokenizer.tokens {
+		_, isSpecialCharacter := xmlSpecialCharacters[token.token]
+		if isSpecialCharacter {
+			token.token = xmlSpecialCharacters[token.token]
+		}
+		resultBuffer.WriteString(fmt.Sprintf("\n<%s> %s </%s>", token.tokenType, token.token, token.tokenType))
+	}
+
+	resultBuffer.WriteString("\n</tokens>")
+
+	result := resultBuffer.String()
+
+	if expectedResult != result {
+		t.Errorf("The expected result don't match with the result")
+	}
+
+	fmt.Println(resultBuffer.String())
 }
