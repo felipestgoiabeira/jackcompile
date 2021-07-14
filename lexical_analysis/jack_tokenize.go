3b84e05a0f3e6faa2fd1ee9360089ea29ef8d336
felipestgoiabeira <felipesg.12@outlook.com>
2021-07-14 11:47:58 -0300

    feat jack_tonize: helper functions to compile engine

--- lexical_analysis/jack_tokenize.go
+++ lexical_analysis/jack_tokenize.go
@@ -8,6 +8,7 @@ import (
 
 type JackTokenizer struct {
 	tokens []Token
+	pos    int32
 }
 
 func NewJackTokenizer(file string) *JackTokenizer {
@@ -29,4 +30,34 @@ func NewJackTokenizer(file string) *JackTokenizer {
 	log.Printf("Tokens found :: %d", len(j.tokens))
 
 	return j
+}
+
+func (j *JackTokenizer) Advance() {
+	j.pos++
+}
+
+func (j *JackTokenizer) HasMoreTokens() bool {
+	posCur := j.pos + 1
+	return !(int(posCur) > len(j.tokens))
+}
+
+func (j *JackTokenizer) HasPeekToken() bool {
+	posCur := j.pos + 2
+	return !(int(posCur) > len(j.tokens))
+}
+
+func (j *JackTokenizer) GetCurToken() Token {
+	return j.tokens[j.pos]
+}
+
+func (j *JackTokenizer) GetCurTokenType() TokenType {
+	return j.tokens[j.pos].GetType()
+}
+
+func (j *JackTokenizer) GetPeekToken() Token {
+	return j.tokens[j.pos+1]
+}
+
+func (j *JackTokenizer) GetPeekTokenType() TokenType {
+	return j.tokens[j.pos+1].GetType()
 }
